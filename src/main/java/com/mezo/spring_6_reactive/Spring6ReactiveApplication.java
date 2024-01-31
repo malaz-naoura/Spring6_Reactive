2ebbbe7fc8455ea139d62e6ddf2f34ccd597f144
malaz_naoura_dell <malaznaoura@gmail.com>
2024-01-31 13:47:54 +0300

    init project

---
+++ src/main/java/com/mezo/spring_6_reactive/Spring6ReactiveApplication.java
@@ -0,0 +1,20 @@
+package com.mezo.spring_6_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+public class Spring6ReactiveApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Spring6ReactiveApplication.class, args);
+
+    }
+
+}
